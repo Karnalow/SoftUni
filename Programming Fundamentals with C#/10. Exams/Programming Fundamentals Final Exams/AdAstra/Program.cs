5e145caa9e3e1f3a284ebceb9c4a5e96233464e6
Ivan Karnalow <ivan.karnalov11@abv.bg>
2021-12-10 16:08:02 +0200

    Add more exams

---
+++ Programming Fundamentals with C#/10. Exams/Programming Fundamentals Final Exams/AdAstra/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"([#]|[|])(?<itemName>[A-Za-z ]+)\1(?<expirationDate>(?<day>\d{2})([\/])(?<mont>\d{2})\5(?<year>\d{2}))\1(?<calories>(?:\d{1,4}|10000))\1";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int totalCalories = 0;
+
+            foreach (Match match in matches)
+            {
+                totalCalories += int.Parse(match.Groups["calories"].Value);
+            }
+
+            int days = totalCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match match in matches)
+            {
+                string itemName = match.Groups["itemName"].Value;
+                string expirationDate = match.Groups["expirationDate"].Value;
+                int calories = int.Parse(match.Groups["calories"].Value);
+
+                Console.WriteLine($"Item: {itemName}, Best before: {expirationDate}, Nutrition: {calories}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/10. Exams/Programming Fundamentals Final Exams/ThePianist/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThePianist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> pieces =
+            new Dictionary<string, Dictionary<string,string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                string piece = input[0];
+                string composer = input[1];
+                string key = input[2];
+
+                pieces[piece] = new Dictionary<string, string>();
+
+                pieces[piece].Add(composer, key);
+            }
+
+            string[] command = Console.ReadLine()
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "Stop")
+            {
+
+                string piece = command[1];
+
+                if (command[0] == "Add")
+                {
+                    string composer = command[2];
+                    string key = command[3];
+
+                    if (!pieces.ContainsKey(piece))
+                    {
+                        pieces[piece] = new Dictionary<string, string>();
+
+                        pieces[piece].Add(composer, key);
+
+                        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{piece} is already in the collection!");
+                    }
+                }
+                else if (command[0] == "Remove")
+                {
+                    if (pieces.ContainsKey(piece))
+                    {
+                        pieces.Remove(piece);
+
+                        Console.WriteLine($"Successfully removed {piece}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+                else if (command[0] == "ChangeKey")
+                {
+                    string newKey = command[2];
+
+                    if (pieces.ContainsKey(piece))
+                    {
+                        var compositors = pieces[piece].Keys.ToList();
+
+                        pieces[piece][compositors[0]] = newKey;
+
+                        Console.WriteLine($"Changed the key of {piece} to {newKey}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+
+                command = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var piece in pieces.OrderBy(p => p.Key).ThenBy(c => c.Value.Keys))
+            {
+                var compositor = piece.Value.Keys.ToList();
+                var key = piece.Value.Values.ToList();
+
+                Console.WriteLine($"{piece.Key} -> Composer: {compositor[0]}, Key: {key[0]}");
+            }
+        }
+    }
+}
