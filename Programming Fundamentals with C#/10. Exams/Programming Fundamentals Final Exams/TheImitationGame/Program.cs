095e2dd04e666845a6761913ebe315bb572ee1c3
Ivan Karnalow <ivan.karnalov11@abv.bg>
2021-12-10 14:19:03 +0200

    Add exercise from exam

---
+++ Programming Fundamentals with C#/10. Exams/Programming Fundamentals Final Exams/TheImitationGame/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace TheImitationGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string[] command = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "Decode")
+            {
+                string cmdArg = command[0];
+
+                if (cmdArg == "Move")
+                {
+                    int numberOfLetters = int.Parse(command[1]);
+
+                    string letters = message.Substring(0, numberOfLetters);
+
+                    message = message.Remove(0, numberOfLetters);
+                    message = message.Insert(message.Length, letters);
+                }
+                else if (cmdArg == "Insert")
+                {
+                    int index = int.Parse(command[1]);
+                    string value = command[2];
+
+                    message = message.Insert(index, value);
+                }
+                else if (cmdArg == "ChangeAll")
+                {
+                    char substring = char.Parse(command[1]);
+                    char replacement = char.Parse(command[2]);
+
+                    foreach (var item in message)
+                    {
+                        if (item == substring)
+                        {
+                            message = message.Replace(substring, replacement);
+                        }
+                    }
+                }
+
+                command = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}
