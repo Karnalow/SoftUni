7fe4999ce8656d937d9dcaa55596c56afcbb8eba
Ivan Karnalow <ivan_karnalov3@mail.bg>
2021-12-14 17:56:25 +0200

    Add queues and stacks more exercises

---
+++ C# Advanced/01. Stacks and Queues/Exercise/SimpleTextEditor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            StringBuilder builder = new StringBuilder();
+            Stack<string> stack = new Stack<string>();
+            stack.Push(builder.ToString());
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string[] cmdArg = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int commandNumber = int.Parse(cmdArg[0]);
+
+                if (commandNumber == 1)
+                {
+                    string argument = cmdArg[1];
+
+                    builder.Append(argument);
+                    stack.Push(builder.ToString());
+                }
+                else if (commandNumber == 2)
+                {
+                    int argument = int.Parse(cmdArg[1]);
+
+                    builder.Remove(builder.Length - argument, argument);
+                    stack.Push(builder.ToString());
+                }
+                else if (commandNumber == 3)
+                {
+                    int argument = int.Parse(cmdArg[1]);
+
+                    Console.WriteLine(builder[argument - 1]);
+                }
+                else if (commandNumber == 4)
+                {
+                    stack.Pop();
+
+                    builder = new StringBuilder();
+
+                    builder.Append(stack.Peek());
+                }
+            }
+        }
+    }
+}
