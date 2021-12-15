0c97a4ee3727679a08203c6094b333fe07803d31
Ivan Karnalow <ivan_karnalov3@mail.bg>
2021-12-15 16:18:03 +0200

    Add exercise with generics

---
+++ C# Advanced/08. Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public void Add(T value)
+        {
+            internalList.Add(value);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return internalList.Count;
+            }
+        }
+
+        public T Remove()
+        {
+            var element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+
+            return element;
+        }
+    }
+}
