dad8e8c816d6909892e9a6828e721d6af3d4d090
Ivan Karnalow <ivan_karnalov3@mail.bg>
2021-12-23 19:54:09 +0200

    Add exercise with inheritance

---
+++ C# OOP/Lab/RandomList/RandomList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string GetRandomElement()
+        {
+            int index = random.Next(0, this.Count);
+
+            return this[index];
+        }
+
+        public void RemoveRandomElement()
+        {
+            int index = random.Next(0, this.Count);
+            RemoveAt(index);
+        }
+    }
+}
