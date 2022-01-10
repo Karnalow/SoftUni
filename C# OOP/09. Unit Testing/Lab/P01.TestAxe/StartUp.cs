7e0b87a0f3646bbf15c3a7ae0a2fbf2c835b173f
Ivan Karnalow <ivan_karnalov3@mail.bg>
2022-01-10 15:29:33 +0200

    Add unit testing: lab exercises

---
+++ C# OOP/09. Unit Testing/Lab/P01.TestAxe/StartUp.cs
@@ -0,0 +1,9 @@
+﻿namespace P01.TestAxe
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C# OOP/09. Unit Testing/Lab/UnitTesting.Tests/DummyTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using P01.TestAxe.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyIsDummyLosesHealth()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.AreNotEqual(10, dummy.Health);
+        }
+
+        [Test]
+        public void DummyCanGiveXP()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            if (dummy.IsDead())
+            {
+                Assert.AreEqual(10, dummy.GiveExperience());
+            }
+        }
+
+        [Test]
+        public void DummyIfDummyIsDeadCanNotBeAttacked()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            if (dummy.IsDead())
+            {
+                throw new System.Exception("Dummy is dead.");
+            }
+            else
+            {
+                axe.Attack(dummy);
+            }
+        }
+
+        [Test]
+        public void DummyIfDummyIsAliveCanNotGiveXP()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            if (!dummy.IsDead())
+            {
+                throw new System.Exception("Dummy is alive and cant give XP");
+            }
+        }
+    }
+}
