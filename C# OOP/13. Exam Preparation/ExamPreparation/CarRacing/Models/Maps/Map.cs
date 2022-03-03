95ff508237d73b875ac45239aff9423c1fe832e6
Karnalow <95851197+Karnalow@users.noreply.github.com>
2022-03-03 13:13:06 +0200

    Add exam preparation for C# OOP

---
+++ C# OOP/13. Exam Preparation/ExamPreparation/CarRacing/Models/Maps/Map.cs
@@ -0,0 +1,66 @@
+﻿using CarRacing.Models.Maps.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Maps
+{
+    public class Map : IMap
+    {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                return "Race cannot be completed because both racers are not available!";
+            }
+            else if (!racerOne.IsAvailable() && racerTwo.IsAvailable())
+            {
+                return $"{racerTwo.Username} wins the race! {racerOne.Username} was not available to race!";
+            }
+            else if (racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                return $"{racerOne.Username} wins the race! {racerTwo.Username} was not available to race!";
+            }
+
+            racerOne.Race();
+            racerTwo.Race();
+
+            IRacer winner;
+
+            double firstRacerChance = racerOne.Car.HorsePower * racerOne.DrivingExperience;
+
+            if (racerOne.RacingBehavior == "strict")
+            {
+                firstRacerChance *= 1.2;
+            }
+            else if (racerOne.RacingBehavior == "aggressive")
+            {
+                firstRacerChance *= 1.1;
+            }
+
+            double secondRacerChance = racerTwo.Car.HorsePower * racerTwo.DrivingExperience;
+
+            if (racerTwo.RacingBehavior == "strict")
+            {
+                secondRacerChance *= 1.2;
+            }
+            else if (racerTwo.RacingBehavior == "aggressive")
+            {
+                secondRacerChance *= 1.1;
+            }
+
+            if (firstRacerChance > secondRacerChance)
+            {
+                winner = racerOne;
+            }
+            else
+            {
+                winner = racerTwo;
+            }
+
+            return $"{racerOne.Username} has just raced against {racerTwo.Username}!" +
+                    $" {winner.Username} is the winner!";
+        }
+    }
+}
