335151f3f19a3e9e9e8620dc0ff8d61963b12455
Karnalow <95851197+Karnalow@users.noreply.github.com>
2022-05-28 22:08:51 +0300

    Add exercises from stream, files and directories lab

---
+++ C# Advanced/04. Streams, Files and Directories/Lab/P01.OddLines/StarUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace P01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\Repositories\SoftUni\C# Advanced\04. Streams, Files and Directories\Lab\P01.OddLines\Data\input.txt");
+
+            StreamWriter sw = new StreamWriter(@"D:\Repositories\SoftUni\C# Advanced\04. Streams, Files and Directories\Lab\P01.OddLines\Data\output.txt");
+
+            string line = sr.ReadLine();
+            int counter = 0;
+
+            while (line != null)
+            {
+                if (counter % 2 != 0)
+                {
+                    sw.WriteLine(line);
+                }
+
+                counter++;
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ C# Advanced/04. Streams, Files and Directories/Lab/P02.LineNumbers/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace P02.LineNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\Repositories\SoftUni\C# Advanced\04. Streams, Files and Directories\Lab\P02.LineNumbers\Data\Input.txt");
+
+            StreamWriter sw = new StreamWriter(@"D:\Repositories\SoftUni\C# Advanced\04. Streams, Files and Directories\Lab\P02.LineNumbers\Data\Output.txt");
+
+            string line = sr.ReadLine();
+            int counter = 1;
+
+            while (line != null)
+            {
+                sw.WriteLine($"{counter}.{line}");
+
+                counter++;
+                line = sr.ReadLine();
+            }
+
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
