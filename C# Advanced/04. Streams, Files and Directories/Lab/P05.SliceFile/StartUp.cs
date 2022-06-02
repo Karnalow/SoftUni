4d43b0f1020d6ef7d3909cd1ad5fe045a08be067
Karnalow <95851197+Karnalow@users.noreply.github.com>
2022-06-02 22:38:50 +0300

    Add exercises from stream, files and directories lab

---
+++ C# Advanced/04. Streams, Files and Directories/Lab/P05.SliceFile/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace P05.SliceFile
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fileStream = new FileStream("sliceMe.txt", FileMode.OpenOrCreate);
+
+            byte[] data = new byte[fileStream.Length];
+
+            var bytesPerFile = (int)Math.Ceiling(fileStream.Length / 4.0);
+
+            for (int i = 1; i <= 4; i++)
+            {
+                byte[] buffer = new byte[bytesPerFile];
+                fileStream.Read(buffer);
+
+                using FileStream writer = new FileStream($"Part-{i}.txt", FileMode.OpenOrCreate);
+                writer.Write(buffer);
+            }
+        }
+    }
+}
