05b0d80acdcab775662929c159176b9ad152d269
Karnalow <95851197+Karnalow@users.noreply.github.com>
2022-06-06 22:17:35 +0300

    Add exercises from stream, files and directories lab

---
+++ C# Advanced/04. Streams, Files and Directories/Lab/P06.FolderSize/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace P06.FolderSize
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using StreamWriter sr = new StreamWriter(@"D:\Repositories\SoftUni\C# Advanced\04. Streams, Files and Directories\Lab\P06.FolderSize\Output.txt");
+
+            string directoryPath = @"D:\Repositories\SoftUni\C# Advanced\04. Streams, Files and Directories\Lab\P06.FolderSize\TestFolder";
+
+            string[] files = Directory.GetFiles(directoryPath);
+
+            long totalLenght = 0;
+
+            foreach (string file in files)
+            {
+                totalLenght += new FileInfo(file).Length;
+            }
+
+            sr.Write(totalLenght);
+        }
+    }
+}
