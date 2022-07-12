51a87e8d2b321996628c43bd91b1623a0a2f0b84
Karnalow <95851197+Karnalow@users.noreply.github.com>
2022-07-12 21:45:16 +0300

    Add custom list and stack

---
+++ C# Advanced/07. Workshop/Exercise/CustomList/List.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace CustomList
+{
+    public class List
+    {
+        private const int DefaultCapacity = 2;
+
+        private int[] items;
+
+        public int this[int i]
+        {
+            get
+            {
+                IsInRange(i);
+
+                return items[i];
+            }
+            set
+            {
+                if (i < 0 || i >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                items[i] = value;
+            }
+        }
+
+        public List()
+        {
+            items = new int[DefaultCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(int element)
+        {
+            Resize();
+
+            items[Count++] = element;
+        }
+
+        public int RemoveAt(int index)
+        {
+            IsInRange(index);
+
+            int element = items[index];
+            items[index] = 0;
+
+            for (int i = index; i < Count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            Count--;
+
+            Shrink();
+
+            return element;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        private void Shrink()
+        {
+            if (Count <= items.Length / 4)
+            {
+                int[] tempArray = new int[items.Length / 2];
+
+                for (int i = 0; i < Count; i++)
+                {
+                    tempArray[i] = items[i];
+                }
+
+                items = tempArray;
+            }
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 0 ||
+                firstIndex >= Count ||
+                secondIndex < 0 ||
+                secondIndex >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int tempElement = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = tempElement;
+        }
+
+        private void IsInRange(int i)
+        {
+            if (i < 0 || i >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        private void Resize()
+        {
+            if (Count == items.Length)
+            {
+                int[] tempArray = new int[items.Length * 2];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    tempArray[i] = items[i];
+                }
+
+                items = tempArray;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(String.Join(' ', items));
+        }
+    }
+}
---
+++ C# Advanced/07. Workshop/Exercise/CustomStack/Stack.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class Stack
+    {
+        private const int InitialCapacity = 4;
+
+        private int[] elemets;
+
+        public Stack()
+        {
+            elemets = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(int element)
+        {
+            Resize();
+
+
+            elemets[Count++] = element;
+        }
+
+        public int Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int element = elemets[Count - 1];
+
+            Count--;
+
+            Shrink();
+            
+            return element;
+        }
+
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int element = elemets[Count - 1];
+
+            return element;
+        }
+
+        private void Resize()
+        {
+            if (Count == elemets.Length)
+            {
+                int[] copyArray = new int[elemets.Length * 2];
+
+                for (int i = 0; i < elemets.Length; i++)
+                {
+                    copyArray[i] = elemets[i];
+                }
+
+                elemets = copyArray;
+            }
+        }
+
+        private void Shrink()
+        {
+            if (Count <= elemets.Length / 4)
+            {
+                int[] tempArray = new int[elemets.Length / 2];
+
+                for (int i = 0; i < Count; i++)
+                {
+                    tempArray[i] = elemets[i];
+                }
+
+                elemets = tempArray;
+            }
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < elemets.Length - 1; i++)
+            {
+                action(elemets[i]);
+            }
+        }
+    }
+}
