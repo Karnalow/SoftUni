ab9d19ff436354ffe251c463623745871688eae4
Karnalow <95851197+Karnalow@users.noreply.github.com>
2022-07-13 17:03:31 +0300

    Add custom queue

---
+++ C# Advanced/07. Workshop/Exercise/CustomQueue/Queue.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace CustomQueue
+{
+    public class Queue
+    {
+        private const int InitialCapacity = 4;
+        private const int FirstElementIndex = 0;
+
+        private int[] items;
+        private int count;
+
+        public Queue()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public void Enqueue(int element)
+        {
+            Resize();
+
+            items[count] = element;
+
+            count++;
+        }
+
+        public int Dequeue()
+        {
+            int firstElement = items[FirstElementIndex];
+
+            IsEmpty();
+            SwitchElements();
+
+            count--;
+
+            return firstElement;
+        }
+
+        public int Peek() => items[FirstElementIndex];
+
+        public void Clear()
+        {
+            IsEmpty();
+
+            int[] tempArray = new int[items.Length];
+
+            items = tempArray;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        private void Resize()
+        {
+            if (count == items.Length)
+            {
+                int[] tempArray = new int[items.Length * 2];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    tempArray[i] = items[i];
+                }
+
+                items = tempArray;
+            }
+        }
+
+        private void IsEmpty()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private void SwitchElements()
+        {
+            items[FirstElementIndex] = default;
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                items[i - 1] = items[i];
+            }
+
+            items[items.Length - 1] = default;
+        }
+    }
+}
---
+++ C# Advanced/07. Workshop/Exercise/CustomQueue/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomQueue
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Dequeue();
+            queue.Dequeue();
+
+            queue.Clear();
+
+            queue.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
