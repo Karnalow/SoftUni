00cd1c344fddbb221dc28110972b82cdeeb4a5f7
Karnalow <95851197+Karnalow@users.noreply.github.com>
2023-02-12 12:05:25 +0200

    Update Exam Preparation-16 October 2021.sql

--- MS SQL/09. Exam Preparations/Exam Preparation-16 October 2021.sql
+++ MS SQL/09. Exam Preparations/Exam Preparation-16 October 2021.sql
@@ -82,3 +82,109 @@ DROP CONSTRAINT FK__Clients__Address__33D4B598
 
 DELETE FROM Addresses
 WHERE Country LIKE 'C%'
+
+--P05
+SELECT
+	CigarName,
+	PriceForSingleCigar,
+	ImageURL
+FROM Cigars
+ORDER BY PriceForSingleCigar ASC, CigarName DESC
+
+--P06
+SELECT
+	c.Id,
+	c.CigarName,
+	c.PriceForSingleCigar,
+	t.TasteType,
+	t.TasteStrength
+FROM Cigars c
+	JOIN Tastes t ON t.Id = c.TastId
+WHERE t.TasteType = 'Earthy' OR t.TasteType = 'Woody'
+ORDER BY c.PriceForSingleCigar DESC
+
+--P07
+SELECT
+	c.Id,
+	c.FirstName + ' ' + c.LastName AS [ClientName],
+	c.Email
+FROM Clients c
+	LEFT JOIN ClientsCigars cc ON cc.ClientId = c.Id
+WHERE cc.ClientId IS NULL
+ORDER BY [ClientName] ASC
+
+--P08
+SELECT TOP(5)
+	c.CigarName,
+	c.PriceForSingleCigar,
+	c.ImageURL
+FROM Cigars c
+	JOIN Sizes s ON s.Id = c.SizeId
+WHERE (s.[Length] >= 12 AND c.CigarName LIKE '%ci%') OR 
+	  (c.PriceForSingleCigar > 50 AND s.RingRange > 2.55)
+ORDER BY c.CigarName ASC, c.PriceForSingleCigar DESC
+
+--P09
+SELECT
+	c.FirstName + ' ' + c.LastName AS [FullName],
+	a.Country,
+	a.ZIP,
+	'$' + CAST(MAX(ci.PriceForSingleCigar) AS VARCHAR) AS [CigarPrice]
+FROM Clients c
+	JOIN ClientsCigars cc ON cc.ClientId = c.Id
+	JOIN Cigars ci ON ci.Id = cc.CigarId
+	JOIN Addresses a ON a.Id = c.AddressId
+WHERE a.ZIP NOT LIKE '%[^0-9]%'
+GROUP BY c.FirstName, c.LastName, a.Country, a.ZIP
+ORDER BY [FullName] ASC
+
+--P10
+SELECT
+	c.LastName,
+	AVG(s.[Length]) AS [CiagrLength],
+	CEILING(s.RingRange) AS [CiagrRingRange]
+FROM Clients c
+	JOIN ClientsCigars cc ON cc.ClientId = c.Id
+	JOIN Cigars ci ON ci.Id = cc.CigarId
+	JOIN Sizes s ON s.Id = ci.SizeId
+WHERE cc.ClientId IS NOT NULL
+GROUP BY c.LastName, s.RingRange
+ORDER BY [CiagrLength] DESC
+
+--P11
+CREATE FUNCTION udf_ClientWithCigars(@name VARCHAR(30))
+RETURNS INT
+AS
+BEGIN
+	DECLARE @numbersOfCigars INT = (SELECT
+										COUNT(*)
+									FROM Clients c
+										JOIN ClientsCigars cc ON cc.ClientId = c.Id
+										JOIN Cigars ci ON ci.Id = cc.CigarId
+									WHERE c.FirstName = @name)
+
+	RETURN @numbersOfCigars
+END
+
+SELECT dbo.udf_ClientWithCigars('Betty')
+
+--P12
+CREATE PROC usp_SearchByTaste(@taste VARCHAR(20))
+AS
+BEGIN
+	SELECT
+		c.CigarName,
+		'$' + CAST(c.PriceForSingleCigar AS VARCHAR(MAX)) AS [Price],
+		t.TasteType,
+		b.BrandName,
+		CAST(s.[Length] AS VARCHAR(MAX)) + ' cm' AS [CigarLength],
+		CAST(s.RingRange AS VARCHAR(MAX)) + ' cm' AS [CigarRingRange]
+	FROM Cigars c
+		JOIN Sizes s ON s.Id = c.SizeId
+		JOIN Tastes t ON t.Id = c.TastId
+		JOIN Brands b ON b.Id = c.BrandId
+	WHERE t.TasteType = @taste
+	ORDER BY [CigarLength] ASC, [CigarRingRange] DESC
+END
+
+EXEC usp_SearchByTaste 'Woody'
