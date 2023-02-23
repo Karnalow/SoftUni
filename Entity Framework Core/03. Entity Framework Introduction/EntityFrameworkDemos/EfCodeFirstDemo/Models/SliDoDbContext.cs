99f4aa8c2f923ceb7476d419fb01164c7e58147e
Karnalow <95851197+Karnalow@users.noreply.github.com>
2023-02-23 19:42:35 +0200

    Update ef intoduction demos

---
+++ Entity Framework Core/03. Entity Framework Introduction/EntityFrameworkDemos/EfCodeFirstDemo/Models/SliDoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirstDemo.Models
+{
+    public class SliDoDbContext : DbContext
+    {
+        public SliDoDbContext()
+        {
+            
+        }
+
+        public SliDoDbContext(DbContextOptions dbContextOptions)
+            :base(dbContextOptions)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=SliDo;Integrated Security=true");
+            }
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
---
+++ Entity Framework Core/03. Entity Framework Introduction/EntityFrameworkDemos/EfCodeFirstDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using EfCodeFirstDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirstDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var db = new SliDoDbContext();
+            db.Database.Migrate();
+        }
+    }
+}
