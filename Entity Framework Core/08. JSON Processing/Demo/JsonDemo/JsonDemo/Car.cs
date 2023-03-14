a2da9ab47b6ea878d54dbd95d121e8809fb12c77
Karnalow <95851197+Karnalow@users.noreply.github.com>
2023-03-14 18:18:42 +0200

    Add demo for json processing

---
+++ Entity Framework Core/08. JSON Processing/Demo/JsonDemo/JsonDemo/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace JsonDemo
+{
+    public class Car
+    {
+        public string Vendor { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime ManufacturedOn { get; set; }
+
+        public List<string> Extras { get; set; }
+
+        public Engine Engine { get; set; }
+    }
+}
---
+++ Entity Framework Core/08. JSON Processing/Demo/JsonDemo/JsonDemo/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonDemo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var car = new Car
+            {
+                Extras = new List<string> { "Klimatronik", "Quattro", "Farove" },
+                ManufacturedOn = DateTime.Now,
+                Model = "A4",
+                Vendor = "Audi",
+                Price = 5000.50M,
+                Engine = new Engine { Volume = 2.5F, HorsePower = 180 }
+            };
+
+            //File.WriteAllText("myCar.json", JsonSerializer.Serialize(car));
+
+            //var options = new JsonSerializerOptions
+            //{
+            //    WriteIndented = true
+            //};
+
+            //Console.WriteLine(JsonSerializer.Serialize(car, options));
+
+            //var json = File.ReadAllText("myCar.json");
+            //
+            //var car = JsonSerializer.Deserialize<Car>(json);
+
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new DefaultNamingStrategy()
+                }
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(car, settings));
+        }
+    }
+}
