74e0fc3ed79ca9c1386af919ad58ae9c8e2b05fb
Karnalow <95851197+Karnalow@users.noreply.github.com>
2023-03-29 21:01:09 +0300

    Add best practices and architecture lab

---
+++ Entity Framework Core/10. Best Practices And Architecture/Lab/RealEstates/RealEstates.ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Models;
+
+namespace RealEstates.ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var db = new ApplicationDbContext();
+            db.Database.Migrate();
+
+            db.Districts.Add(new District { Name = "Bracigovo"});
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Entity Framework Core/10. Best Practices And Architecture/Lab/RealEstates/RealEstates.Importer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstates.Importer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
