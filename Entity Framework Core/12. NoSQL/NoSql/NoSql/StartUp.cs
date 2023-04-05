91209f1c81321feca06a3a244864fc1c036f966a
Karnalow <95851197+Karnalow@users.noreply.github.com>
2023-04-05 19:17:19 +0300

    Add noslq exercises

---
+++ Entity Framework Core/12. NoSQL/NoSql/NoSql/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace NoSql
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+
+            IMongoDatabase database = client.GetDatabase("NoSQL");
+
+            var collection = database.GetCollection<BsonDocument>("softuniArticles");
+
+            var allArticles = collection.Find(new Article { }).ToList();
+
+            foreach (var article in allArticles) 
+            {
+                string name = article.GetElement("name").Value.AsString;
+
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
