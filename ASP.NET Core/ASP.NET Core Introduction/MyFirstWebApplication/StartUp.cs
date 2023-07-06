f9bafbe894d0ab537ebb36e0721ed2cfeffdc49a
Karnalow <95851197+Karnalow@users.noreply.github.com>
2023-07-06 22:30:44 +0300

    Add ASP.NET Core Introduction

---
+++ ASP.NET Core/ASP.NET Core Introduction/MyFirstWebApplication/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace MyFirstWebApplication
+{
+    public class StartUp
+    {
+        public void ConfigureServices(IServiceCollection services)
+        { 
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+                endpoints.MapGet("/ivan", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Ivan!");
+                });
+            });
+        }
+    }
+}
